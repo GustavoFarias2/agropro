84fe329bcf8982a9c8419118484e883d697f0ef2
BuildTools <gustavo.fariassiqueira@gmail.com>
2020-09-03 18:29:48 -0300

    header responsivity

--- agropro-web/src/components/Header/index.js
+++ agropro-web/src/components/Header/index.js
@@ -13,32 +13,45 @@ const Header = ({ routes = [], setRoute }) => {
 
   return (
 
-    <Layout.Header title='Agropro'>
+    <Layout.Header
+      title='Agropro'
+      style={{ padding: '0 25px' }}
+    >
       <Row>
 
-        <Col>
-          <span style={{
-            fontSize: 24,
-            color: '#e1e1e1',
-            marginRight: 30,
-          }}>
+        <Col
+          xs={4}
+          sm={4}
+        >
+          <span style={{ fontSize: 24, color: '#e1e1e1', marginRight: 30 }}>
             Agropro
-        </span>
+          </span>
         </Col>
 
-        <Col>
-          <Menu theme="dark" mode="horizontal">
-
+        <Col
+          xs={2}
+          sm={2}
+          md={22}
+          flex='auto'
+        >
+          <Menu theme="dark" mode='horizontal'>
             {routes.map((route, i) =>
-              <Menu.Item
-                key={i}
-                onClick={() => handleRouteChange(route)}
-              >
+              <Menu.Item key={i} onClick={() => handleRouteChange(route)}>
                 {route}
               </Menu.Item>
             )}
-
           </Menu>
+        </Col>
+
+        <Col
+          xs={18}
+          sm={18}
+        >
+          <div style={{ textAlign: 'right' }}>
+            <a href='#'>
+              Logout
+            </a>
+          </div>
         </Col>
 
       </Row>
