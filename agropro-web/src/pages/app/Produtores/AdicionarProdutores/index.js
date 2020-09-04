ac28c7b51566b9810561062ced772fed14d07125
BuildTools <gustavo.fariassiqueira@gmail.com>
2020-09-04 18:25:44 -0300

    fazenda form component added

--- agropro-web/src/pages/app/Produtores/AdicionarProdutores/index.js
+++ agropro-web/src/pages/app/Produtores/AdicionarProdutores/index.js
@@ -11,13 +11,12 @@ import {
   Card,
   Form,
   Input,
-  InputNumber,
-  Select
+  InputNumber
 } from 'antd';
 
-import { PlusCircleOutlined, MinusCircleOutlined } from '@ant-design/icons';
+import { PlusCircleOutlined } from '@ant-design/icons';
 
-import estados from '../../../../assets/estados';
+import FazendaForm from './fazendaForm';
 
 const AdicionarProdutores = ({ setRoute, formId, setFormId }) => {
 
@@ -136,46 +135,12 @@ const AdicionarProdutores = ({ setRoute, formId, setFormId }) => {
               />
             </Row>
 
-            {fazendas.map((fazenda, i) => (
-              <Row key={i} style={{ marginTop: 25 }}>
-
-                <Col xs={24} sm={24} md={12} style={{ paddingRight: 20 }}>
-                  <Form.Item name={['fazenda ' + fazenda, 'nome']} label='Nome da Fazenda' rules={[{ required: true }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name={['fazenda ' + fazenda, 'endereco', 'cidade']} label='Cidade' rules={[{ required: true }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name={['fazenda ' + fazenda, 'endereco', 'estado']} label='Estado' rules={[{ required: true }]}>
-                    <Select placeholder='Estado'>
-                      {estados.map((estado, i) => <Select.Option key={i} name={estado.toLocaleLowerCase()}>{estado}</Select.Option>)}
-                    </Select>
-                  </Form.Item>
-                  <Form.Item name={['fazenda ' + fazenda, 'endereco', 'culturas']} label='Culturas' rules={[{ required: true }]}>
-                    <Input />
-                  </Form.Item>
-                </Col>
-
-                <Col xs={24} sm={24} md={12} style={{ paddingRight: 20 }}>
-                  <Form.Item name={['fazenda ' + fazenda, 'area']} label='Área' rules={[{ required: true }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name={['fazenda ' + fazenda, 'area_consolidada']} label='Área Consolidade' rules={[{ required: true }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name={['fazenda ' + fazenda, 'area_legal']} label='Área Legal' rules={[{ required: true }]}>
-                    <Input />
-                  </Form.Item>
-                  <MinusCircleOutlined
-                    onClick={() => handleRemoverFazenda(i)}
-                    style={{ fontSize: '22px', marginLeft: '10px', marginTop: '15px', cursor: 'pointer' }}
-                  />
-                </Col>
-
-                <div style={{ border: '1px solid #f0f0f0', width: '100%' }}></div>
-
-              </Row>
-            ))}
+            {fazendas.map((fazenda, i) =>
+              <FazendaForm
+                fazenda={fazenda}
+                i={i}
+                handleRemoverFazenda={handleRemoverFazenda}
+              />)}
 
           </Form>
         </Card>
